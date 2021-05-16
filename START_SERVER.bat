7021f8a4c8b148deb217e2674c6858cde96f1b61
shygirlgamer <84287372+shygirlgamer@users.noreply.github.com>
2021-05-16 16:14:28 -0400

    updating file

--- START_SERVER.bat
+++ START_SERVER.bat
@@ -8,7 +8,7 @@ set WORLD=%DIR_NAME%\world
 
 for /r %%i in (*.jar) do set SERVER_JAR=%%~nxi
 if exist %WORLD% (
- #java -Xmx1024M -Xms1024M -jar %SERVER_JAR% nogui
+ java -Xmx1024M -Xms1024M -jar %SERVER_JAR% nogui
 ) else (
   echo unable to locate world folder, make sure it is in the same directory
   echo current directory: %cd%
